7795edc124bb58a390999b246e9bf89f8f87c179
SeckMohameth <mohamethseck34@gmail.com>
2024-11-14 14:18:06 -0500

    initali commit with multiple xcode projects

---
+++ SwiftUIText/SwiftUIText/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUIText
+//
+//  Created by Mohameth Seck on 11/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Let's learn")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIText/SwiftUIText/SwiftUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextApp.swift
+//  SwiftUIText
+//
+//  Created by Mohameth Seck on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
